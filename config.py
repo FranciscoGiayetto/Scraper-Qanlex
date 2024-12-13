feb60cc4b6cbd05bc540cb20ca6b01d9ba04ce27
FranciscoGiayetto <frangiayetto@gmail.com>
2024-12-12 22:49:47 -0300

    Añadiendo configuración de la base de datos

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-
+from selenium.webdriver.chrome.service import Service
 # Configuración de Selenium
 def setup_driver():
     options = webdriver.ChromeOptions()
@@ -7,3 +7,14 @@ def setup_driver():
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     return driver
+
+# Configuración de la base de datos
+DATABASE_CONFIG = {
+    "host": "127.0.0.1",
+    "user": "root",
+    "password": "pepe1234",
+    "database": "expedientes",
+    "port": 3306,
+    "connect_timeout": 10,
+}
+
